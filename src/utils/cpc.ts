00ac8ae722d463857cd9d69cfc9fa2a5be61ab4e
Mao <37288931+mao0507@users.noreply.github.com>
2024-04-07 03:50:55 +0800

    feat:調整字串

--- src/utils/cpc.ts
+++ src/utils/cpc.ts
@@ -1,7 +1,7 @@
 import { Bot } from '../utils/telegramBot';
 import { cpcApi } from '../service/cpc';
 
-export const get_cpc = async (messageId: number, chatId: number) => {
+export const getCpc = async (messageId: number, chatId: number) => {
   const d = await cpcApi();
 
   const option = {
@@ -16,13 +16,13 @@ export const get_cpc = async (messageId: number, chatId: number) => {
   str += `單位：元/公升 \n`;
   str += `本週汽油價格 ${isDowngrade ? '調降' : '調漲'} ${price} \n`;
   str += `從 ${d.PriceUpdate} 零時起實施 \n`;
-  str += `92 無鉛汽油 : ${d.sPrice1} \n`;
-  str += `95 無鉛汽油 : ${d.sPrice2} \n`;
-  str += `98 無鉛汽油 : ${d.sPrice3} \n`;
-  str += `超級柴油 : ${d.sPrice5} \n`;
+  str += `92 無鉛汽油 : ${d.sPrice1} 元 \n`;
+  str += `95 無鉛汽油 : ${d.sPrice2} 元 \n`;
+  str += `98 無鉛汽油 : ${d.sPrice3} 元 \n`;
+  str += `超級柴油 : ${d.sPrice5} 元 \n`;
   str += `價格如有變動，請依現場實際牌價為主 `;
-  // str += `酒精汽油 : ${d.sPrice4} \n`;
-  // str += `液化石油氣 : ${d.sPrice6} \n`;
+  // str += `酒精汽油 : ${d.sPrice4} 元 \n`;
+  // str += `液化石油氣 : ${d.sPrice6} 元 \n`;
 
   Bot.editMessageText(str, option);
 };
