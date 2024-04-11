bdc6153a0e1bdb70c85bd89aedad2d66b0d82c58
Mao <37288931+mao0507@users.noreply.github.com>
2024-04-12 02:39:04 +0800

    feat:處理主要機器人對應事件，處發函式

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,63 @@
-import express from 'express';
+import { Bot } from './utils/telegramBot';
+import { inlineButton } from './utils/inlineButton';
+import { getCpc } from './utils/cpc';
+import { getNpc } from './utils/npc';
+import { getSmile } from './utils/smile';
 
-const app: any = express();
+//提示訊息，方便終端機檢閱是否執行
+console.log('telegramBot is working Now');
 
-const port: number = 3000;
+// 中油
+Bot.onText(/\/cpc/, async (msg) => {
+  const userId = msg.chat.id;
 
-app.get('/', (req: any, res: any) => {
-  res.send('helloWorld');
+  const { message_id, chat } = await Bot.sendMessage(userId, '資料取得中...');
+
+  await getCpc(message_id, chat.id);
 });
 
-app.listen(port, () => {
-  console.log(`server is listening on ${port} !!!`);
+// 全國加油站
+Bot.onText(/\/npc/, async (msg) => {
+  const userId = msg.chat.id;
+
+  const { message_id, chat } = await Bot.sendMessage(userId, '資料取得中...');
+
+  await getNpc(message_id, chat.id);
+});
+
+// 速邁樂
+Bot.onText(/\/smile/, async (msg) => {
+  const userId = msg.chat.id;
+
+  const { message_id, chat } = await Bot.sendMessage(userId, '資料取得中...');
+
+  await getSmile(message_id, chat.id);
+});
+
+// 選單按鈕
+Bot.onText(/\/start/, (msg) => {
+  const userId = msg.chat.id;
+  Bot.sendMessage(userId, '請選擇要查看的油價資訊 ⛽', {
+    reply_markup: inlineButton,
+  });
+});
+
+// 按鈕觸發後，接收回傳資訊
+Bot.on('callback_query', async (d) => {
+  const { message_id, chat } = await Bot.sendMessage(
+    d.from.id,
+    '資料取得中...'
+  );
+
+  if (d.data == '/cpc') {
+    await getCpc(message_id, chat.id);
+  }
+
+  if (d.data == '/npc') {
+    await getNpc(message_id, chat.id);
+  }
+
+  if (d.data == '/smile') {
+    await getSmile(message_id, chat.id);
+  }
 });
